333b52977bb7627e2d08d0a5312741413bf585fd
Arthur Gunn <arthur@gunn.co.nz>
2010-12-13 23:30:56 +1300

    Created a Node.set method, used this to refactor the node classes.

--- lib/vitae/server/node.rb
+++ lib/vitae/server/node.rb
@@ -1,44 +1,59 @@
 class Node
   include Haml::Helpers
   include Helpers
-  
+
   attr_reader :level, :name, :data, :path
-  
-  def self.types
-    {
-      "tag_cloud" => TagCloudNode,
-      "project_list" => ProjectListNode,
-      "project" => ProjectNode,
-      "basic" => Node
-    }
+
+  class << self    
+    def set(option, value=self, &block)
+      value = block if block
+      if value.kind_of?(Proc)
+        define_method option, &value
+      elsif value == self && option.respond_to?(:each)
+        option.each { |k,v| set(k, v) }
+      else
+        set option, Proc.new{value}
+      end
+      self
+    end
+
+    def types
+      {
+        "tag_cloud" => TagCloudNode,
+        "project_list" => ProjectListNode,
+        "project" => ProjectNode,
+        "base" => BaseNode,
+        "standard" => Node
+      }
+    end
   end
-  
+
   def initialize(data, name=nil, level=1, parent_path=[])
     @data = data
     @name = name
     @level = level
     @path = parent_path << name
-    
+  
     init_haml_helpers
   end
-  
+
   def to_s
     capture_haml do
       html
     end
   end
-  
+
   def html
     haml_tag "h#{level}", name if name
     show_data
   end
-  
+
   def output_data
     data.except(%w[node_type])
   end
-  
+
   def show_data
-    case output_data
+    case data
     when Hash
       show_hash
     when Array
@@ -47,18 +62,34 @@ class Node
       haml_concat data
     end
   end
-  
+
+  def collection_wrapper &block
+    return block.call unless collection_tag
+    haml_tag collection_tag, &block
+  end
+
+  def node_wrapper &block
+    return block.call unless node_tag
+    haml_tag node_tag, &block
+  end
+
+  set :collection_tag => "ul", :node_tag => "li"
+
   def show_hash
-    output_data.each do |key, value|
-      node_class = child_node_class(key, value)
-      haml_concat node_class.new(value, key, level+1, path)
+    collection_wrapper do
+      output_data.each do |key, value|
+        node_class = child_node_class(key, value)
+        node_wrapper do
+          haml_concat node_class.new(value, key, level+1, path)
+        end
+      end
     end
   end
-  
+
   def child_node_class_from_yaml(value)
     Node.types[value["node_type"]]
   end
-  
+
   def child_node_class_from_name(name)
     case name
     when /expertise/i then TagCloudNode
@@ -66,61 +97,63 @@ class Node
     else Node
     end
   end
-  
+
   def child_node_class(key, value)
-    child_node_class_from_yaml(value) || child_node_class_from_name(key)
+    child_node_class_from_yaml(value) || default_child_node_class || child_node_class_from_name(key)
   end
+  set :default_child_node_class, nil
+
+end
+
+%w[ProjectNode TagCloudNode ProjectListNode BaseNode].each do |class_name|
+  Object.const_set(class_name, Class.new(Node))
+end
+
+class BaseNode < Node
+  set :collection_tag, nil
+  set :node_tag, nil
 end
 
 class TagCloudNode < Node
-  def html
-    haml_tag "h#{level}", name
-    haml_tag "ul#expertise.tags" do
-      show_hash
-    end
-  end
+  set :collection_tag, "ul#expertise.tags"
   
   def show_hash
-    output_data.each do |key, value|
-      haml_tag "li.skill", key, :class => value
+    collection_wrapper do
+      output_data.each do |key, value|
+        haml_tag "li.skill", key, :class => value
+      end
     end
   end
 end
 
 class ProjectListNode < Node
-  def show_hash
-    haml_tag "ul.jobs-list.vcalendar" do
-      output_data.each do |key, value|
-        haml_concat ProjectNode.new(value, key, level+1, path)
-      end
-    end
-  end
+  set :collection_tag, "ul.jobs-list.vcalendar"
+  set :node_tag, "li.vevent.experience"
+  set :default_child_node_class, ProjectNode
 end
 
 class ProjectNode < Node
   def html
-    haml_tag "li.vevent.experience" do
-      link = link_to(name, data["url"]) if data["url"]
-      haml_tag "h#{level}" do
-        haml_tag "span.location", (link||name)
-        if data["role"]
-          haml_concat " - "
-          haml_tag "span.summary", data["role"]
-        end
-        show_date_range
+    link = link_to(name, data["url"]) if data["url"]
+    haml_tag "h#{level}" do
+      haml_tag "span.location", (link||name)
+      if data["role"]
+        haml_concat " - "
+        haml_tag "span.summary", data["role"]
       end
-      
-      haml_tag "p.job-description", data["description"]
+      show_date_range
     end
+  
+    haml_tag "p.job-description", data["description"]
     # show_data
   end
-  
+
   def show_date_range
     return unless data["start"] || data["end"]
-    
+  
     start_text = data["start"] ? data["start"].strftime("%B %Y") : nil
     end_text   = data["end"]   ? data["end"].strftime("%B %Y")   : "Present"
-    
+  
     haml_tag "span.dtrange" do
       haml_tag "span.dtstart", start_text, :title => data["start"] if start_text
       haml_concat "-"
@@ -128,22 +161,3 @@ class ProjectNode < Node
     end
   end
 end
-
-# class NodeMap
-#   def method_name
-#     
-#   end
-#   
-#   
-#   def node_for_path path
-#     VCardNode
-#     ProjectNode
-#     Node
-#     
-#     ["contact"], VCardNode
-#     ["projects", "*"] => ProjectNode
-#     ["*"] => Node
-#     
-#     Customer = Struct.new(:path, :node_class)
-#   end
-# end
